578856c45a2ca536a6d37bb75e8a4275bd21fa67
Karol Topor <toporkarol@gmail.com>
2026-09-22 23:47:30 +0200

    hypr: updated keybindings

--- hypr/.config/hypr/modules/keybindings.lua
+++ hypr/.config/hypr/modules/keybindings.lua
@@ -5,16 +5,16 @@ local vars = require("modules.vars")
 ---------------------
 ------ MONITORS ------
 ---------------------
-hl.bind("CTRL + ALT + " .. vars.keypad[1] , hl.dsp.dpms({ action = "toggle", monitor = vars.monitors.bottom }))
-hl.bind("CTRL + ALT + " .. vars.keypad[2] , hl.dsp.dpms({ action = "toggle", monitor = vars.monitors.side }))
-hl.bind("CTRL + ALT + " .. vars.keypad[3] , hl.dsp.dpms({ action = "toggle", monitor = vars.monitors.top }))
+hl.bind("CTRL + ALT + " .. vars.keypad[1], hl.dsp.dpms({ action = "toggle", monitor = vars.monitors.bottom }))
+hl.bind("CTRL + ALT + " .. vars.keypad[2], hl.dsp.dpms({ action = "toggle", monitor = vars.monitors.side }))
+hl.bind("CTRL + ALT + " .. vars.keypad[3], hl.dsp.dpms({ action = "toggle", monitor = vars.monitors.top }))
 
 --------------------
 --- APPLICATIONS ---
 --------------------
 
 hl.bind("CTRL + ALT + t", hl.dsp.exec_cmd("kitty"))
-hl.bind(vars.keys.mainMod .. "+ ALT + R", hl.dsp.exec_cmd("command -v hyprshutdown >/dev/null 2>&1 && hyprshutdown || hyprctl dispatch 'hl.dsp.exit()'"))
+hl.bind(vars.keys.mainMod .. "+ ALT + R",hl.dsp.exec_cmd("command -v hyprshutdown >/dev/null 2>&1 && hyprshutdown || hyprctl dispatch 'hl.dsp.exit()'"))
 hl.bind(vars.keys.mainMod .. " + ALT + L", hl.dsp.exec_cmd("hyprlock"))
 hl.bind(vars.keys.mainMod .. " + E", hl.dsp.exec_cmd(vars.apps.fileManager .. " --new-window"))
 hl.bind(vars.keys.mainMod .. " + SPACE", hl.dsp.exec_cmd(vars.apps.menu))
@@ -31,8 +31,8 @@ hl.bind(vars.keys.mainMod .. " + SHIFT + s", hl.dsp.exec_cmd("hyprshot -m region
 ------------------------------
 --- WINDOWS AND WORKSPACES ---
 ------------------------------
--- Maximize windows 
-hl.bind(vars.keys.mainMod .. " + M",  hl.dsp.window.fullscreen({ mode = "maximized", action = "toggle" }))
+-- Maximize windows
+hl.bind(vars.keys.mainMod .. " + M", hl.dsp.window.fullscreen({ mode = "maximized", action = "toggle" }))
 -- Move focus with mainMod + [h/j/k/l]
 hl.bind(vars.keys.mainMod .. " + h", hl.dsp.focus({ direction = "left" }))
 hl.bind(vars.keys.mainMod .. " + l", hl.dsp.focus({ direction = "right" }))
@@ -64,27 +64,21 @@ hl.bind(vars.keys.mainMod .. " + CTRL + RIGHT", hl.dsp.window.resize({ x = 50, y
 hl.bind(vars.keys.mainMod .. " + CTRL + UP", hl.dsp.window.resize({ x = 0, y = -50, relative = true }))
 hl.bind(vars.keys.mainMod .. " + CTRL + DOWN", hl.dsp.window.resize({ x = 0, y = 50, relative = true }))
 
--- Focus and Move with Numpad 
+-- Focus and Move with Numpad
 for i = 1, 10 do
-    local key = i % 10 -- still for the main number row
+	local key = i % 10 -- still for the main number row
 
-    -- main row: SUPER + [0-9]
-    hl.bind(vars.keys.mainMod .. " + " .. key,
-        hl.dsp.focus({ workspace = i }))
+	-- main row: SUPER + [0-9]
+	hl.bind(vars.keys.mainMod .. " + " .. key, hl.dsp.focus({ workspace = i }))
+	-- main row move
+	hl.bind(vars.keys.mainMod .. " + SHIFT + " .. key, hl.dsp.window.move({ workspace = i }))
 
-    -- main row move
-    hl.bind(vars.keys.mainMod .. " + SHIFT + " .. key,
-        hl.dsp.window.move({ workspace = i }))
-
-    -- numpad: SUPER + KP_*
-    local kp = vars.keypad[i]
-    if kp then
-        hl.bind(vars.keys.mainMod .. " + " .. kp,
-            hl.dsp.focus({ workspace = i }))
-
-        hl.bind(vars.keys.mainMod .. " + SHIFT + " .. kp,
-            hl.dsp.window.move({ workspace = i }))
-    end
+	-- numpad: SUPER + KP_*
+	local kp = vars.keypad[i]
+	if kp then
+		hl.bind(vars.keys.mainMod .. " + " .. kp, hl.dsp.focus({ workspace = i }))
+		hl.bind(vars.keys.mainMod .. " + SHIFT + " .. kp, hl.dsp.window.move({ workspace = i }))
+	end
 end
 
 -- Move/resize windows with mainMod + LMB/RMB and dragging
@@ -92,11 +86,11 @@ hl.bind(vars.keys.mainMod .. " + mouse:272", hl.dsp.window.drag(), { mouse = tru
 hl.bind(vars.keys.mainMod .. " + mouse:273", hl.dsp.window.resize(), { mouse = true })
 -- Scroll through existing workspaces with mainMod + scroll
 hl.bind(vars.keys.mainMod .. " + mouse_down", hl.dsp.focus({ workspace = "e+1" }))
-hl.bind(vars.keys.mainMod .. " + mouse_up",   hl.dsp.focus({ workspace = "e-1" }))
+hl.bind(vars.keys.mainMod .. " + mouse_up", hl.dsp.focus({ workspace = "e-1" }))
 -- Move focused window to next/previous workspace with mainMod + scroll
 hl.bind(vars.keys.mainMod .. " + SHIFT + mouse_down", hl.dsp.window.move({ workspace = "e+1" }))
 hl.bind(vars.keys.mainMod .. " + SHIFT + mouse_up", hl.dsp.window.move({ workspace = "e-1" }))
--- logitech mouse side buttons 
+-- logitech mouse side buttons
 hl.bind(vars.keys.mainMod .. " + mouse:275", hl.dsp.focus({ workspace = "e-1" })) -- back
 hl.bind(vars.keys.mainMod .. " + mouse:276", hl.dsp.focus({ workspace = "e+1" })) -- forward
 
@@ -117,21 +111,33 @@ hl.bind(vars.keys.mainMod .. " + ALT + X", hl.dsp.workspace.toggle_special("term
 --- KEYBOARD ---
 ----------------
 -- Keyboard multimedia keys for volume and LCD brightness
-hl.bind("XF86AudioRaiseVolume", hl.dsp.exec_cmd("wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"),
-    { locked = true, repeating = true })
-hl.bind("XF86AudioLowerVolume", hl.dsp.exec_cmd("wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"),
-    { locked = true, repeating = true })
-hl.bind("XF86AudioMute", hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"),
-    { locked = true, repeating = true })
-hl.bind("XF86AudioMicMute", hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"),
-    { locked = true, repeating = true })
+hl.bind(
+	"XF86AudioRaiseVolume",
+	hl.dsp.exec_cmd("wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"),
+	{ locked = true, repeating = true }
+)
+hl.bind(
+	"XF86AudioLowerVolume",
+	hl.dsp.exec_cmd("wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"),
+	{ locked = true, repeating = true }
+)
+hl.bind(
+	"XF86AudioMute",
+	hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"),
+	{ locked = true, repeating = true }
+)
+hl.bind(
+	"XF86AudioMicMute",
+	hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"),
+	{ locked = true, repeating = true }
+)
 hl.bind("XF86MonBrightnessUp", hl.dsp.exec_cmd("brightnessctl -e4 -n2 set 5%+"), { locked = true, repeating = true })
 hl.bind("XF86MonBrightnessDown", hl.dsp.exec_cmd("brightnessctl -e4 -n2 set 5%-"), { locked = true, repeating = true })
 ---- Requires playerctl
-hl.bind("XF86AudioNext", hl.dsp.exec_cmd("playerctl next"), { locked = true })
-hl.bind("XF86AudioPause", hl.dsp.exec_cmd("playerctl play-pause"), { locked = true })
-hl.bind("XF86AudioPlay", hl.dsp.exec_cmd("playerctl play-pause"), { locked = true })
-hl.bind("XF86AudioPrev", hl.dsp.exec_cmd("playerctl previous"), { locked = true })
+hl.bind("XF86AudioNext", hl.dsp.exec_cmd("playerctl -p spotify next"), { locked = true })
+hl.bind("XF86AudioPause", hl.dsp.exec_cmd("playerctl -p spotify play-pause"), { locked = true })
+hl.bind("XF86AudioPlay", hl.dsp.exec_cmd("playerctl -p spotify play-pause"), { locked = true })
+hl.bind("XF86AudioPrev", hl.dsp.exec_cmd("playerctl -p spotify previous"), { locked = true })
 
 -- Example special workspace (scratchpad)
 -- hl.bind(vars.keys.mainMod .. " + S",         hl.dsp.workspace.toggle_special("magic"))
